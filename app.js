2502745dac92b1fb3d9a6743f2032704c04f9e3d
Francisco Cesare <franciscocesare@MacBook-Pro-de-Francisco.local>
2022-06-14 18:29:56 -0300

    agregue formulario registro y login, sus views y varios estilos. faltan detalles

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-})   
+  console.log("Server is running on http://localhost:3000");
+});
